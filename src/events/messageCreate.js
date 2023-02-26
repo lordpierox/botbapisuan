ced1ffc219c158ee1985aff5813df2a34a8c7812
Deraktsu <pierolosi21@gmail.com>
2023-02-26 19:41:42 +0100

    Update messageCreate.js

--- src/events/messageCreate.js
+++ src/events/messageCreate.js
@@ -8,7 +8,7 @@ module.exports = {
     name: 'messageCreate',
     on: true,
     async execute(client, message, messageCreate) {
-        if (client.type == 20 && client.channel.id == "1032780435425603614" && client.interaction.commandName == "asushin") {
+        if (client.type == 20 && client.channel.id == "1032780435425603614" && client.interaction.commandName == "bump") {
             //console.log(client)
             try{
                 console.log("bump detectado")
