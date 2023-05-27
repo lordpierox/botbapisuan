2e22392cd9e15dce4c876539991c6ccc61baf575
Deraktsu <pierolosi21@gmail.com>
2023-05-27 19:25:32 +0200

    Update invite.js

--- src/commands/Community/invite.js
+++ src/commands/Community/invite.js
@@ -8,25 +8,13 @@ module.exports = {
 	async execute(interaction, client, message) {
 
 		
-		var invites = []; // starting array
-		message.client.guilds.cache.forEach(async (guild) => { // iterate loop on each guild bot is in
-	
-		  // get the first channel that appears from that discord, because
-		  // `.createInvite()` is a method for a channel, not a guild.
-		  const channel = guild.channels.cache 
-			.filter((channel) => channel.type === 'text')
-			.first();
-		  if (!channel || guild.member(client.user).hasPermission('CREATE_INSTANT_INVITE')) return;
-		  await channel
-			.createInvite({ maxAge: 0, maxUses: 0 })
-			.then(async (invite) => {
-			  invites.push(`${guild.name} - ${invite.url}`); // push invite link and guild name to array
-			})
-			.catch((error) => console.log(error));
-		  console.log(invites);
-		});
+		await client.guilds.cache.forEach(guild => {
+            var invite = interaction.guild.invites.create(interaction.channel,{maxAge: 0}).then((invite) => console.log("https://discord.gg/"+invite.code+"  "))
+            
+            //console.log(`${guild.name} | ${guild.id}`);
+          })  
 
-		await interaction.reply({ content: 'Mensaje enviado!', ephemeral: true });
+		await interaction.reply({ content: 'Mensaje enviado!', ephemeral: false });
 
 	
 	},
