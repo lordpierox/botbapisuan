c58f0b4946b4305384ff2e606967204c75ab2d3a
Deraktsu <ploartesilvestre@gmail.com>
2025-10-13 22:18:12 +0200

    Update bite.js

--- src/commands/Actions/bite.js
+++ src/commands/Actions/bite.js
@@ -28,7 +28,7 @@ module.exports = {
 
 
         console.log("numero di choice: "+ choice[ran]);
-    let bite = new EmbedBuilder()
+    let kiss = new EmbedBuilder()
          
          .setDescription(answer)
          .setColor("Random")
@@ -36,6 +36,6 @@ module.exports = {
          .setImage(choice[ran])
          .setFooter({ text:"SukiBot"});
         
-    interaction.followUp({ embeds: [bite] })
+    interaction.followUp({ embeds: [kiss] })
     },
 }
